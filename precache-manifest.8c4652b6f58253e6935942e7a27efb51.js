e7b04ac188a3b5f65950eb8478991d3c1ea5b91a
Khyber Sen <kkysen@gmail.com>
2019-06-25 22:49:48 -0400

    Updates

---
+++ precache-manifest.8c4652b6f58253e6935942e7a27efb51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b1ba3e8f42d6b92b95d5c5ef911c22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fe6111b056da33fb63e",
+    "url": "/static/css/main.cc535bf0.chunk.css"
+  },
+  {
+    "revision": "1bd1368d99bc46de0e92",
+    "url": "/static/js/2.17df3e80.chunk.js"
+  },
+  {
+    "revision": "7fe6111b056da33fb63e",
+    "url": "/static/js/main.8b18d894.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
