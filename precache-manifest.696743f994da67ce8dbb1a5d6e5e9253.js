6b6f10f15fe5b470f5ece5e9071760bf7f266724
Khyber Sen <kkysen@gmail.com>
2019-06-27 15:26:40 -0400

    Updates

---
+++ precache-manifest.696743f994da67ce8dbb1a5d6e5e9253.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2024c9ba3edecc4655fad54e5d50af8",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "265440bf853f7f2aafcc",
+    "url": "/me/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "872c44c4143eb35bb275",
+    "url": "/me/static/js/2.b344f31a.chunk.js"
+  },
+  {
+    "revision": "265440bf853f7f2aafcc",
+    "url": "/me/static/js/main.e9827176.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  },
+  {
+    "revision": "f968b40c1263b4b7ee0d3186873e781e",
+    "url": "/me/static/media/N Queens Animation.f968b40c.gif"
+  },
+  {
+    "revision": "17e9dd485b9e47c6772546128ee72d6e",
+    "url": "/me/static/media/N Queens Solved.17e9dd48.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/me/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.1150e799cf0cb1f258d835336ab53027.js"
+  "/me/precache-manifest.696743f994da67ce8dbb1a5d6e5e9253.js"
 );
 
 self.addEventListener('message', (event) => {
